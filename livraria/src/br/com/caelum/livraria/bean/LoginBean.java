3accd3231f7045fad5a3e1146a3a69903d49a18b
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-27 09:08:58 -0300

    Final aula 01 Integração do CDI com JSF 2

--- livraria/src/br/com/caelum/livraria/bean/LoginBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,19 +2,24 @@ package br.com.caelum.livraria.bean;
 
 
 
+import java.io.Serializable;
+
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
 
-@ManagedBean
+@Named
 @ViewScoped
-public class LoginBean {
+public class LoginBean implements Serializable {
 
-    private Usuario usuario = new Usuario();
+	private static final long serialVersionUID = 2039182637211948417L;
+	
+	private Usuario usuario = new Usuario();
     
     public String efetuaLogin() {
         System.out.println("Fazendo login do usuário "
--- livraria/src/br/com/caelum/livraria/bean/TemaBean.java
+++ livraria/src/br/com/caelum/livraria/bean/TemaBean.java
@@ -1,12 +1,16 @@
 package br.com.caelum.livraria.bean;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 
-@ManagedBean
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
 @SessionScoped
-public class TemaBean {
+public class TemaBean implements Serializable{
 
+	private static final long serialVersionUID = -2871493857147685220L;
+	
 	private String tema = "vader";
 
 	public String getTema() {
@@ -18,14 +22,11 @@ public class TemaBean {
 	}
 
 	public String[] getTemas() {
-		return new String[] { "afterdark", "afternoon", "afterwork", "aristo",
-				"black-tie", "blitzer", "bluesky", "bootstrap", "casablanca",
-				"cupertino", "cruze", "dark-hive", "delta", "dot-luv",
-				"eggplant", "excite-bike", "flick", "glass-x", "home",
-				"hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
-				"overcast", "pepper-grinder", "redmond", "rocket", "sam",
-				"smoothness", "south-street", "start", "sunny", "swanky-purse",
-				"trontastic", "ui-darkness", "ui-lightness", "vader" };
+		return new String[] { "afterdark", "afternoon", "afterwork", "aristo", "black-tie", "blitzer", "bluesky",
+				"bootstrap", "casablanca", "cupertino", "cruze", "dark-hive", "delta", "dot-luv", "eggplant",
+				"excite-bike", "flick", "glass-x", "home", "hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
+				"overcast", "pepper-grinder", "redmond", "rocket", "sam", "smoothness", "south-street", "start",
+				"sunny", "swanky-purse", "trontastic", "ui-darkness", "ui-lightness", "vader" };
 	}
 
 }
