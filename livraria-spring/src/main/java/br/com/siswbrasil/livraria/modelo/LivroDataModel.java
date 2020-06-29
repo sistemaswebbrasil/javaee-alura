5d5b274cf07f869a0791c239280cf213ea499a3b
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-29 10:47:26 -0300

    Projeto livraria duplicado , mas este novo usando Maven e Spring

---
+++ livraria-spring/src/main/java/br/com/siswbrasil/livraria/modelo/LivroDataModel.java
@@ -0,0 +1,41 @@
+package br.com.siswbrasil.livraria.modelo;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+
+import org.primefaces.model.FilterMeta;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortMeta;
+import org.springframework.stereotype.Component;
+
+import br.com.siswbrasil.livraria.dao.LivroDao;
+
+@Component
+@ViewScoped
+public class LivroDataModel extends LazyDataModel<Livro> {
+
+	private static final long serialVersionUID = 2948232237230849780L;
+
+	@Inject
+	private LivroDao dao;
+
+	@Override
+	public List<Livro> load(int first, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
+
+		try {
+			List<Livro> livroList = dao.primeFacesFilter(first, pageSize, sortBy, filterBy);
+			int rowCount = dao.primeFacesFilterCount(filterBy);
+			super.setRowCount(rowCount);
+			return livroList;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+}
