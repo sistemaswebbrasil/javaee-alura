b6a224d8b1f776e94c3e7c18a035e349ad4ebd27
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-30 21:46:18 -0300

    Final AULA 05 Conhecendo e Utilizando Cache no JavaEE

--- casadocodigo/src/main/java/br/com/siswbrasil/dao/LivroDao.java
+++ casadocodigo/src/main/java/br/com/siswbrasil/dao/LivroDao.java
@@ -3,6 +3,7 @@ package br.com.siswbrasil.dao;
 import java.util.List;
 
 import javax.ejb.Stateful;
+import javax.persistence.Cache;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
@@ -14,48 +15,41 @@ import br.com.siswbrasil.entity.Livro;
 @Stateful
 public class LivroDao {
 
-	@PersistenceContext(type=PersistenceContextType.EXTENDED)
+	@PersistenceContext(type = PersistenceContextType.EXTENDED)
 	private EntityManager manager;
-	
+
 	public void salvar(Livro livro) {
 		manager.persist(livro);
 	}
 
+	public void limpaCache() {
+		Cache cache = manager.getEntityManagerFactory().getCache();
+		cache.evict(Livro.class);
+	}
+
 	public List<Livro> listar() {
-		String jpql = "select distinct(l) from Livro l "
-				+ " join fetch l.autores";
-		
+		String jpql = "select distinct(l) from Livro l " + " join fetch l.autores";
+
 		return manager.createQuery(jpql, Livro.class).getResultList();
 	}
 
 	public List<Livro> ultimosLancamentos() {
-		String jpql = "select l from Livro l "
-				+ "where l.capaPath is not null "
-				+ "order by l.id desc";
-		return manager.createQuery(jpql, Livro.class)
-				.setMaxResults(5)
-				.setHint(QueryHints.CACHEABLE, true)
+		String jpql = "select l from Livro l " + "where l.capaPath is not null " + "order by l.id desc";
+		return manager.createQuery(jpql, Livro.class).setMaxResults(5).setHint(QueryHints.CACHEABLE, true)
 				.getResultList();
 	}
 
 	public List<Livro> demaisLivros() {
-		String jpql = "select l from Livro l  "
-				+ "where l.capaPath is not null "
-				+ "order by l.id desc";
-		return manager.createQuery(jpql, Livro.class)
-				.setFirstResult(5)
-				.setHint(QueryHints.CACHEABLE, true)
+		String jpql = "select l from Livro l  " + "where l.capaPath is not null " + "order by l.id desc";
+		return manager.createQuery(jpql, Livro.class).setFirstResult(5).setHint(QueryHints.CACHEABLE, true)
 				.getResultList();
 	}
 
 	public Livro buscarPorId(Integer id) {
-//		return manager.find(Livro.class, id);
-		
-		String jpql = "select l from Livro l join fetch l.autores "
-				+ "where l.id = :id";
-		return manager.createQuery(jpql, Livro.class)
-				.setParameter("id", id)
-				.getSingleResult();
+		// return manager.find(Livro.class, id);
+
+		String jpql = "select l from Livro l join fetch l.autores " + "where l.id = :id";
+		return manager.createQuery(jpql, Livro.class).setParameter("id", id).getSingleResult();
 	}
-	
+
 }
